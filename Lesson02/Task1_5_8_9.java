348f1e5740434bda4a2895ecf54496abb81f2657
Tanis138 <tanis138@gmail.com>
2019-06-26 12:18:10 +0300

    [add] Tasks 1.5, 1.8, 1.9

---
+++ Lesson02/Task1_5_8_9.java
@@ -0,0 +1,14 @@
+public class Task1_5_8_9 {
+
+    public static void main(String[] args) {
+        System.out.println("1.5 Вывести на одной строке числа 1, 13 и 49 с одним пробелом между ними:");
+        System.out.println(1 + " " + 13 + " " + 49);
+
+        System.out.println("\n1.8 Вывод на экран в одной строки четырех любых чисел с одним пробелом между ними:");
+        int n1 = 1, n2 = 2, n3 = 3, n4 = 4;
+        System.out.printf("%d %d %d %d%n", n1, n2, n3, n4);
+
+        System.out.println("\n1.9 Вывести на экран числа 50 и 10 одно под другим:");
+        System.out.println(5 + "\n" + 10);
+    }
+}
