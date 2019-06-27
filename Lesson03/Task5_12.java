3f55cdf3edd4f68bbb7085a989cae543d75ccb17
Tanis138 <tanis138@gmail.com>
2019-06-27 21:26:21 +0300

    [add] Task 5.12

---
+++ Lesson03/Task5_12.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Task5_12 {
+
+    public static void main(String[] args) {
+        final double P0 = 1.29;
+        final double z = 1.25 / 10000;
+        double p;
+        
+        System.out.println("Таблица зависимости плотности воздуха от высоты (м):");
+        for (int h = 0; h <= 1000; h += 100) {
+            System.out.printf("%dm\t", h);
+        }
+        System.out.println();
+        for (int h = 0; h <= 1000; h += 100) {
+            p = P0 * Math.exp(-h * z);
+            System.out.printf("%.3f\t", p);
+        }
+    }
+}
