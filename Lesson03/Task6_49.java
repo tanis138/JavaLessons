986ab36cb84199299998146e8322eb9f2726765e
Tanis138 <tanis138@gmail.com>
2019-06-28 14:56:11 +0300

    [add] Task 6.49

---
+++ Lesson03/Task6_49.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Task6_49 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите натуральное число n: ");
+        int n = in.nextInt();
+
+        int dig;
+        boolean has2 = false, has3 = false, has5 = false;
+        do {
+            dig = n % 10;
+            switch (dig) {
+                case 2:
+                    has2 = true;
+                    break;
+                case 3:
+                    has3 = true;
+                    break;
+                case 5:
+                    has5 = true;
+                    break;
+            }
+            n /= 10;
+        } while (n >= 10);
+
+        if (has2)
+            System.out.println("В этом числе есть цифра 2");
+        if (has3 && has5)
+            System.out.println("В этом числе есть цифры 3 и 5");
+
+        in.close();
+    }
+}
