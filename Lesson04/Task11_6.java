ee30bd457abcd750e0cd7173c6a4c0dd1fb09c2c
Tanis138 <tanis138@gmail.com>
2019-07-02 20:52:22 +0300

    [add] Task 11.6

---
+++ Lesson04/Task11_6.java
@@ -0,0 +1,10 @@
+public class Task11_6 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[12];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+            System.out.printf("%d ",arr[i]);
+        }
+    }
+}
