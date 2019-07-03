13af89fb9ac1641f524cdf24526a0c5d73addf7c
Tanis138 <tanis138@gmail.com>
2019-07-03 12:54:50 +0300

    [add] Task 11.101

---
+++ Lesson04/Task11_101.java
@@ -0,0 +1,62 @@
+public class Task11_101 {
+
+    public static void main(String[] args) {
+        final int COUNT = 10;
+        int[] a = new int[COUNT];
+        int[] cnt = new int[COUNT];
+
+        System.out.println("Исходный массив:");
+        for (int i = 0; i < COUNT; i++) {
+            a[i] = (int) (Math.random() * 50);
+            System.out.printf("%d\t", a[i]);
+        }
+        System.out.println("\n");
+
+        // считаем все повторения элементов в массиве
+        // все повторяющиееся индексы кроме первого делаем -1
+        for (int i = 0; i < COUNT; i++) {
+            for (int j = i; j < COUNT; j++) {
+                if (cnt[j] < 0) {
+                    continue;
+                }
+                if (a[i] == a[j]) {
+                    cnt[i]++;
+                    if (i != j) {
+                        cnt[j] = -1;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Массив повторений элементов:");
+        for (int i : cnt) {
+            System.out.printf("%d\t", i);
+        }
+        System.out.println("\n");
+
+        // ищем пару одинаковых элементов
+        // если их > 3 присваеваем количеству -1
+        int pairCount = 0;
+        String strPairs = "";
+        for (int i = 0; i < COUNT; i++) {
+            if (cnt[i] > 2) {
+                pairCount = -1;
+                break;
+            }
+            if (cnt[i] == 2) {
+                pairCount++;
+                strPairs += a[i] + " ";
+            }
+        }
+
+        if (pairCount < 0) {
+            System.out.println("В массиве имеется больше двух одинаковых элементов.");
+        } else if (pairCount == 0) {
+            System.out.printf("В массиве нет ни одной пары одинаковых элементов.%n");
+        } else if (pairCount == 1) {
+            System.out.printf("В массиве только одна пара одинаковых элементов: %s%n", strPairs);
+        } else {
+            System.out.printf("В массиве несколько пар из двух одинаковых элементов: %s", strPairs);
+        }
+    }
+}
--- Lesson04/Task11_53.java
+++ Lesson04/Task11_53.java
@@ -6,36 +6,41 @@ public class Task11_53 {
         int[] a1 = new int[COUNT];
         int[] a2 = new int[COUNT];
         int[] a3 = new int[COUNT];
-        int n = 3, m = 4;
+        int m = 4, n = 3;
 
         System.out.println("Исходный массив:");
         for (int i = 0; i < COUNT; i++) {
             a[i] = (int) (Math.random() * 100);
             a1[i] = (a[i] % 10 == 0) ? 0 : a[i];
-            a2[i] = (a[i] % 2 == 0) ? a[i] / 2 : a[i] * 2;
-            a3[i] = (a[i] % 2 == 0) ? a[i] : a[i] - m;
+            if (a[i] % 2 == 0) {
+                a2[i] = a[i] / 2;
+                a3[i] = a[i];
+            } else {
+                a2[i] = a[i] * 2;
+                a3[i] = a[i] - m;
+            }
             if (i % 2 == 0) {
                 a3[i] += n;
             }
-            System.out.printf("%d ", a[i]);
+            System.out.printf("%d\t", a[i]);
         }
-        System.out.println();
+        System.out.println("\n");
 
         System.out.println("Все элементы, кратные числу 10, заменить нулем:");
         for (int i : a1) {
-            System.out.printf("%d ", i);
+            System.out.printf("%d\t", i);
         }
-        System.out.println();
+        System.out.println("\n");
 
         System.out.println("Все нечетные элементы удвоить, а четные уменьшить вдвое:");
         for (int i : a2) {
-            System.out.printf("%d ", i);
+            System.out.printf("%d\t", i);
         }
-        System.out.println();
+        System.out.println("\n");
 
         System.out.printf("Нечетные элементы уменьшить на %d, а элементы с нечетными номерами увеличить на %d:%n", m, n);
         for (int i : a3) {
-            System.out.printf("%d ", i);
+            System.out.printf("%d\t", i);
         }
         System.out.println();
     }
