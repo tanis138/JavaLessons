a6251f9e25cdea119786585095f708669cc79504
Tanis138 <tanis138@gmail.com>
2019-07-24 16:10:14 +0300

    [Task 21] currentTimeMillis -> nanoTime

--- Lesson08/Task_21.java
+++ Lesson08/Task_21.java
@@ -1,38 +1,39 @@
 public class Task_21 {
 
     public static void main(String[] args) {
-        final String STR = "Hello, world!";
+        final String STR = "Hello!";
         final int COUNT = 50_000;
+        final double NS_TO_MS_DIV = 1000D * 1000D;
 
         long startTime, stopTime;
         
         String s = "";
-        startTime = System.currentTimeMillis();
+        startTime = System.nanoTime();
         for (int i = 0; i < COUNT; i++) {
             s += STR;
         }
-        stopTime = System.currentTimeMillis();
-        long sRes = stopTime - startTime; 
-        System.out.printf("Конкатенация %d строк через String:        %dms\n", COUNT, sRes);
+        stopTime = System.nanoTime();
+        double sRes = (stopTime - startTime) / NS_TO_MS_DIV;
+        System.out.printf("Конкатенация %d строк через String:        %.0fмс\n", COUNT, sRes);
 
         StringBuffer sbf = new StringBuffer();
-        startTime = System.currentTimeMillis();
+        startTime =System.nanoTime();
         for (int i = 0; i < COUNT; i++) {
             sbf.append(STR);
         }
-        stopTime = System.currentTimeMillis();
-        long sbfRes = stopTime - startTime; 
-        System.out.printf("Конкатенация %d строк через StringBuffer:  %dms\n", COUNT, sbfRes);
+        stopTime = System.nanoTime();
+        double sbfRes = (stopTime - startTime) / NS_TO_MS_DIV; 
+        System.out.printf("Конкатенация %d строк через StringBuffer:  %.2fмс\n", COUNT, sbfRes);
 
         StringBuilder sbd = new StringBuilder();
-        startTime = System.currentTimeMillis();
+        startTime = System.nanoTime();
         for (int i = 0; i < COUNT; i++) {
             sbd.append(STR);
         }
-        stopTime = System.currentTimeMillis();
-        long sbdRes = stopTime - startTime; 
-        System.out.printf("Конкатенация %d строк через StringBuilder: %dms\n", COUNT, sbdRes);
+        stopTime = System.nanoTime();
+        double sbdRes = (stopTime - startTime) / NS_TO_MS_DIV; 
+        System.out.printf("Конкатенация %d строк через StringBuilder: %.2fмс\n", COUNT, sbdRes);
 
-        System.out.printf("StringBuilder быстрее String в %d раз", sRes / sbdRes);
+        System.out.printf("StringBuilder быстрее String в %.1f раз", sRes / sbdRes);
     }
 }
