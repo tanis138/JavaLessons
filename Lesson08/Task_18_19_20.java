15b64d358b56832701530760ec514eea7d1f4f94
Tanis138 <tanis138@gmail.com>
2019-07-24 17:39:39 +0300

    [Task 18, 19, 20]  added String.split()

--- Lesson08/Task_18_19_20.java
+++ Lesson08/Task_18_19_20.java
@@ -19,7 +19,7 @@ public class Task_18_19_20 {
     public static int getWordCount(String text) {
         int res = 0;
 
-        String regex = "([A-Za-zА-Яа-я]+)(\\p{Punct}|\\p{Blank})";
+        String regex = "([A-Za-zА-Яа-я]+)(\\p{Punct}|\\p{Blank})?";
 
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(text);
@@ -33,7 +33,7 @@ public class Task_18_19_20 {
     public static String getLastLetters(String text) {
         StringBuilder sb = new StringBuilder();
 
-        String regex = "([A-Za-zА-Яа-я]+)(?=\\p{Punct}|\\p{Blank})";
+        String regex = "([A-Za-zА-Яа-я]+)(?=\\p{Punct}|\\p{Blank})?";
 
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(text);
@@ -48,7 +48,7 @@ public class Task_18_19_20 {
     }
 
     public static void main(String[] args) {
-        String str = "  :Hello,world! Как\tдела? \"Здесь\" - работа с: строками; угу.  ";
+        String str = "  :Hello,world! Как\tдела?! \"Здесь\" - работа с: строками; угу. j  ";
         System.out.printf("Исходная строка: [%s]\n", str);
         System.out.println();
 
@@ -84,6 +84,10 @@ public class Task_18_19_20 {
                     isNewWordFound = true;
                     wordCount++;
                 }
+                // если входная строка заканчивается буквой
+                if (i == str.length()-1) {
+                    sbLastLetters.append(str.charAt(i));
+                }
                 break;
             }
         }
@@ -98,6 +102,30 @@ public class Task_18_19_20 {
         System.out.printf("\tКоличество знаков препинания: %d\n", getPunctCount(str));
         System.out.printf("\tКоличество слов: %d\n", getWordCount(str));
         System.out.printf("\tСтрока из последних букв слов: %s\n", getLastLetters(str));
+        System.out.println();
+
+        System.out.println("Решение через String.split():");
+        
+        punctCount = 0;
+        for (String s : str.split("([A-Za-zА-Яа-я]+)|(\\p{Blank}+)")) {
+            punctCount += s.length();
+            //System.out.printf("[%s] ", s);
+        }
+        System.out.printf("\tКоличество знаков препинания: %d\n", punctCount);
+
+        String[] strs = str.split("(\\p{Punct}+|\\p{Blank}+)");
+        StringBuilder sb = new StringBuilder();
+        wordCount = 0;
+        for (String s : strs) {
+            if (s.length() == 0) {
+                continue;
+            }
+            sb.append(s.charAt(s.length()-1));
+            sb.append(' ');
+            wordCount++;
+        }
+        System.out.printf("\tКоличество слов: %d\n", wordCount);
+        System.out.printf("\tСтрока из последних букв слов: %s\n", sb.toString());
 
     }
 }
